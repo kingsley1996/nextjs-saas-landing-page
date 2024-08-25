884c602b4cd9ff0686ca82d58007047fb40107ba
samuel <linhdang.dev96@gmail.com>
2024-08-25 23:48:26 +0700

    [Feature] Section Product

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,41 @@
+import productImage from "@/assets/product-image.png";
+import Image from "next/image";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} alt="Product Image" className="mt-10" />
+          <Image
+            src={pyramidImage}
+            alt="Pyramid Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+          <Image
+            src={tubeImage}
+            alt="Tube Image"
+            height={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
